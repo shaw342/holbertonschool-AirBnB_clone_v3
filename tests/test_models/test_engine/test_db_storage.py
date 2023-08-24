5f5643465905f64fe8522cbbba07b71d7db7ec38
VotreNom <shawanbarua70@gmail.com>
2023-08-24 17:01:14 +0200

    task

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import inspect
 import models
 from models.engine import db_storage
+from models.engine.db_storage import DBStorage
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -14,6 +15,9 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
 import json
 import os
 import pep8
@@ -28,7 +32,7 @@ class TestDBStorageDocs(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
-        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+        cls.ds_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
     def test_pep8_conformance_db_storage(self):
         """Test that models/engine/db_storage.py conforms to PEP8."""
@@ -59,15 +63,6 @@ test_db_storage.py'])
         self.assertTrue(len(DBStorage.__doc__) >= 1,
                         "DBStorage class needs a docstring")
 
-    def test_dbs_func_docstrings(self):
-        """Test for the presence of docstrings in DBStorage methods"""
-        for func in self.dbs_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
-
-
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
@@ -86,3 +81,30 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test that get properly retrieves one object, None if not found"""
+        from models import storage
+        state = State(name='test')
+        storage.new(state)
+        storage.save()
+
+        obj = storage.get(State, state.id)
+        self.assertEqual(obj, state)
+
+        storage.delete(state)
+        storage.save()
+
+        dummy = storage.get(State, state.id)
+        self.assertIsNone(dummy)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test that count properly gets the number of objects matching"""
+        from models import storage
+        storage_len = len(storage.all())
+        self.assertEqual(storage.count(), storage_len)
+
+        states_nb = len(storage.all(State).values())
+        self.assertEqual(storage.count(State), states_nb)
