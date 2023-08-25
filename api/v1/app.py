1ea7db4d8a494ad57c2423bdda34f6c4e478e5c8
VotreNom <shawanbarua70@gmail.com>
2023-08-25 16:07:47 +0200

    6

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,6 +20,11 @@ def app_tear(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def app_error_not_found(error):
+    return jsonify({'error': 'Not found'}), 404
+
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
