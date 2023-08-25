d77aa87ead74c9b8109896130225c519c915fc03
VotreNom <shawanbarua70@gmail.com>
2023-08-25 16:01:54 +0200

    4

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 
@@ -14,11 +14,16 @@ app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
+@app.teardown_appcontext
+def app_tear(exception):
+    """ call storage.close """
+    storage.close()
+
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
     app.run(
         host=host,
-        port=int(port),
-        threaded=True,
-        debug=True)
+        port=port,
+        threaded=True)
